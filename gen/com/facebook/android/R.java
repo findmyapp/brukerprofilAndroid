2376a009c0525456f1bbeef9f98b6eac9c29a214
Marius <maruz85@hotmail.com>
2011-06-29 13:26:59 +0200

    added .project file

--- gen/com/facebook/android/R.java
+++ gen/com/facebook/android/R.java
@@ -14,11 +14,19 @@ public final class R {
         public static final int facebook_icon=0x7f020000;
         public static final int icon=0x7f020001;
     }
+    public static final class id {
+        public static final int buttonLogin=0x7f050001;
+        public static final int buttonLogout=0x7f050002;
+        public static final int progressLogin=0x7f050003;
+        public static final int textFacebook=0x7f050000;
+    }
     public static final class layout {
         public static final int main=0x7f030000;
     }
     public static final class string {
-        public static final int app_name=0x7f040001;
-        public static final int hello=0x7f040000;
+        public static final int app_name=0x7f040003;
+        public static final int login=0x7f040000;
+        public static final int logout=0x7f040001;
+        public static final int welcome=0x7f040002;
     }
 }
--- src/findmyapp/profile/UserProfileActivity.java
+++ src/findmyapp/profile/UserProfileActivity.java
@@ -1,54 +1,91 @@
 package findmyapp.profile;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.util.ArrayList;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.facebook.android.AsyncFacebookRunner;
+import com.facebook.android.AsyncFacebookRunner.RequestListener;
+import com.facebook.android.DialogError;
+import com.facebook.android.Facebook;
+import com.facebook.android.Facebook.DialogListener;
+import com.facebook.android.FacebookError;
+import com.facebook.android.Util;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import com.facebook.android.*;
-import com.facebook.android.Facebook.*;
+import android.os.Handler;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
 public class UserProfileActivity extends Activity {
 	
-	Facebook facebook = new Facebook("245885202095548");
-	
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        
-        facebook.authorize(this, new DialogListener() {
+	public static final String APP_ID = "245885202095548";
+//	private static final String[] PERMS = new String[] { "user_events" };
+	private Facebook facebook;
+	private AsyncFacebookRunner mAsyncRunner;
 
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		
+		facebook = new Facebook(APP_ID);
+		mAsyncRunner = new AsyncFacebookRunner(facebook);
+		
+		if (APP_ID == null || APP_ID.equals("")) {
+			Util.showAlert(this, "Warning", "Facebook Applicaton ID must be "
+					+ "specified before running");
+		}
+		
+		facebook.authorize(this, new DialogListener() {
 			@Override
 			public void onComplete(Bundle values) {
-				// TODO Auto-generated method stub
-				
+				TextView tv = (TextView) findViewById(R.id.textFacebook);
+				tv.setText("Logged in " + facebook.getAccessToken());
 			}
 
 			@Override
-			public void onFacebookError(FacebookError e) {
-				// TODO Auto-generated method stub
-				
+			public void onFacebookError(FacebookError error) {
+				TextView tv = (TextView) findViewById(R.id.textFacebook);
+				tv.setText("Facebook Error");
+				error.printStackTrace();
 			}
 
 			@Override
 			public void onError(DialogError e) {
-				// TODO Auto-generated method stub
-				
+				TextView tv = (TextView) findViewById(R.id.textFacebook);
+				tv.setText("We have an error!");
+				e.printStackTrace();
 			}
 
 			@Override
 			public void onCancel() {
-				// TODO Auto-generated method stub
-				
+				TextView tv = (TextView) findViewById(R.id.textFacebook);
+				tv.setText("Cancel");
 			}
-        	
-        });
-        
-    }
-    
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
+		});
+		
+	}
 
-        facebook.authorizeCallback(requestCode, resultCode, data);
-    }
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		facebook.authorizeCallback(requestCode, resultCode, data);
+	}
 }
