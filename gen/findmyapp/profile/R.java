5ad8c06168faa8428b4ddf6dee1b6bb0e8957618
Marius <maruz85@hotmail.com>
2011-06-30 16:29:06 +0200

    FB login, logout and passing access token to backend is ready, problem with the httpRequest response, when this is sorted out the response is ready to be parsed and pushed to the UI.

--- gen/findmyapp/profile/R.java
+++ gen/findmyapp/profile/R.java
@@ -12,12 +12,16 @@ public final class R {
     }
     public static final class drawable {
         public static final int facebook_icon=0x7f020000;
-        public static final int icon=0x7f020001;
+        public static final int generic_profile_pic=0x7f020001;
+        public static final int icon=0x7f020002;
     }
     public static final class id {
         public static final int buttonLogin=0x7f050001;
         public static final int buttonLogout=0x7f050002;
-        public static final int progressLogin=0x7f050003;
+        public static final int fbId=0x7f050004;
+        public static final int fbImage=0x7f050003;
+        public static final int fbName=0x7f050005;
+        public static final int progressLogin=0x7f050006;
         public static final int textFacebook=0x7f050000;
     }
     public static final class layout {
@@ -27,6 +31,8 @@ public final class R {
         public static final int app_name=0x7f040003;
         public static final int login=0x7f040000;
         public static final int logout=0x7f040001;
+        public static final int strFbId=0x7f040004;
+        public static final int strFbName=0x7f040005;
         public static final int welcome=0x7f040002;
     }
 }
--- src/findmyapp/profile/UserProfileActivity.java
+++ src/findmyapp/profile/UserProfileActivity.java
@@ -2,13 +2,17 @@ package findmyapp.profile;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
+import java.net.URLEncoder;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.util.EntityUtils;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import com.facebook.android.AsyncFacebookRunner;
 import com.facebook.android.DialogError;
@@ -19,10 +23,12 @@ import com.facebook.android.FacebookError;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class UserProfileActivity extends Activity {
@@ -30,21 +36,26 @@ public class UserProfileActivity extends Activity {
 	public static final String APP_ID = "245885202095548";
 	private static final String[] PERMS = new String[] { "user_events" };
 	private Facebook facebook;
-	private AsyncFacebookRunner mAsyncRunner;
+	
 	private TextView txtStatus;
 	private Button btnLogin;
 	private Button btnLogout;
+	
+//	private String fbinfo = "{ \"id\": \"772612305\", \"name\": \"Torstein M. Barkve\", \"first_name\": \"Torstein\", \"last_name\": \"M. Barkve\", \"link\": \"http://www.facebook.com/profile.php?id=772612305\", \"birthday\": \"05/10/1990\", \"education\": [    {\"school\": { \"id\": \"109956045693499\", \"name\": \"St. Svithun Videreg\u00e5ende Skole\"},\"type\": \"High School\"    },    {\"school\": { \"id\": \"109449622407837\", \"name\": \"Oslo University College\"},\"year\": { \"id\": \"136328419721520\", \"name\": \"2009\"},\"concentration\": [ {    \"id\": \"135775433156356\",    \"name\": \"Anvendt datateknologi\" }],\"type\": \"College\"    } ], \"gender\": \"male\", \"timezone\": 2, \"locale\": \"nn_NO\", \"verified\": true, \"updated_time\": \"2011-06-06T12:18:15+0000\" }";
+	
+	private JSONObject fbJson;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 		
-		final Context c = this;
+		// Save context for use in application, needed for access inside button listeners
+		final Context mainView = this;
 		
 		facebook = new Facebook(APP_ID);
-		mAsyncRunner = new AsyncFacebookRunner(facebook);
 		
+		// Init
 		txtStatus = (TextView) findViewById(R.id.textFacebook);
 		btnLogin = (Button) findViewById(R.id.buttonLogin);
 		btnLogout = (Button) findViewById(R.id.buttonLogout);
@@ -59,23 +70,50 @@ public class UserProfileActivity extends Activity {
 			@Override
 			public void onClick(View v) {
 				if(!facebook.isSessionValid()) {
-					facebook.authorize((Activity) c, new DialogListener() {
+					facebook.authorize((Activity) mainView, new DialogListener() {
 						@Override
 						public void onComplete(Bundle values) {
-							txtStatus.setText("Logged in");
-							// send facebook.getAccessToken() to back end with httpRequest
+							TextView tvFbId = (TextView) findViewById(R.id.fbId);
+						//	String httpRequest = "http://192.168.16.40:8080/findmyapp/auth?accessToken=" + URLEncoder.encode(facebook.getAccessToken());
+							String httpRequest = "http://findmyapp.net/findmyapp/auth?accessToken=" + URLEncoder.encode(facebook.getAccessToken());
 							
 							try {
 								HttpClient hc = new DefaultHttpClient();
-								HttpPost request = new HttpPost("http://www.cuddly-zombie.com");
-								HttpResponse hr = hc.execute(request);
+								HttpGet request = new HttpGet(httpRequest);
+								HttpResponse response = hc.execute(request); // Returns json string with user data
 								
-								if(hr.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
-									txtStatus.setText(EntityUtils.toString(hr.getEntity()));
+								txtStatus.setText("HttpRequest Status Code: " + response.getStatusLine().getStatusCode()); //DEBUG
+								
+								if(response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) { // if status code is 200
+									
+									// Grab json object
+									fbJson = new JSONObject(EntityUtils.toString(response.getEntity()));
+									
+									ImageView imFbImage = (ImageView) findViewById(R.id.fbImage);
+									tvFbId = (TextView) findViewById(R.id.fbId);
+									TextView tvFbName = (TextView) findViewById(R.id.fbName);
+									
+									/* IGNORE
+									// Image cannot be replaced in this way, see example on how to do it:
+									// http://stackoverflow.com/questions/2471935/how-to-load-an-imageview-by-url-in-android
+									String req = "http://graph.facebook.com/" + fbJson.getString("id") + "/picture";
+								//	Uri uri = Uri.parse(facebook.request(req));
+								//	imFbImage.setImageURI(uri);
+								 	*/
+									
+									tvFbId.setText(fbJson.getString("id"));
+									tvFbName.setText(fbJson.getString("name"));
+									
+									txtStatus.setText("Logged in as " + fbJson.getString("name")); //DEBUG
+							//		txtStatus.setText(EntityUtils.toString(hr.getEntity())); // DEBUG
+									
 								}
+
 							} catch (Exception e) {
 								// TODO: handle exception
+								txtStatus.setText(e.getMessage());
 							}
+						//	tvFbId.setText("Test"); // DEBUG
 						}
 
 						@Override
@@ -106,7 +144,7 @@ public class UserProfileActivity extends Activity {
 			public void onClick(View v) {
 				try {
 					if(facebook.isSessionValid()) {
-						facebook.logout(c);
+						facebook.logout(mainView);
 						txtStatus.setText("User has logged out");
 					} else {
 						txtStatus.setText("No user to log out");
@@ -119,7 +157,7 @@ public class UserProfileActivity extends Activity {
 			}
 		});
 	}
-
+	
 	@Override
 	public void onActivityResult(int requestCode, int resultCode, Intent data) {
 		super.onActivityResult(requestCode, resultCode, data);
